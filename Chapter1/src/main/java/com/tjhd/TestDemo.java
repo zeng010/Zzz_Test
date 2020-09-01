356873100be572fa95d086dceba8bedcb3f218df
zeng010 <zeng_h12@163.com>
2020-09-01 23:18:05 +0800

    提交moco

---
+++ Chapter1/src/main/java/com/tjhd/TestDemo.java
@@ -0,0 +1,15 @@
+package com.tjhd;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author: Zzz_tjhd
+ * @date: 2020-09-01 22:20
+ */
+public class TestDemo {
+    @Test
+    public void test(){
+        System.out.println("1");
+    }
+}
+
